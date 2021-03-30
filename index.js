fa086bcd606a143e04c6f23f7830bff3bbce8cab
ZikKim <ziker.kim@gmail.com>
2021-03-31 03:54:28 +0800

    3 files modified

--- index.js
+++ index.js
@@ -11,17 +11,18 @@ import * as firebaseui from "firebaseui";
 
 // Document elements
 const btnLogin = document.getElementById("btnLogin");
-const userContainer = document.getElementById("user-container");
+const btnEdit = document.getElementById("btnEdit");
+//const userContainer = document.getElementById("user-container");
 const menusContainer = document.getElementById("menus-container");
 
-const form = document.getElementById("leave-message");
-const input = document.getElementById("message");
-const guestbook = document.getElementById("guestbook");
-const numberAttending = document.getElementById("number-attending");
-const rsvpYes = document.getElementById("rsvp-yes");
-const rsvpNo = document.getElementById("rsvp-no");
+const frmSearch = document.getElementById("frmSearch");
+//const input = document.getElementById("message");
+//const guestbook = document.getElementById("guestbook");
+//const numberAttending = document.getElementById("number-attending");
+//const rsvpYes = document.getElementById("rsvp-yes");
+//const rsvpNo = document.getElementById("rsvp-no");
+//var rsvpListener = null;
 
-var rsvpListener = null;
 var latestActivitiesListener = null;
 
 async function main() {
@@ -65,6 +66,22 @@ async function main() {
     }
   });
 
+  /*btnEdit.addEventListener("click", () => {
+    console.log("btnEdit");
+  });*/
+
+  frmSearch.addEventListener("submit", e => {
+    var mode = findGetParameter("mode");
+    e.preventDefault();
+    if (mode == "latest") {
+      Latest($("#search_keyword").val());
+    }
+    if (mode == "libcard") {
+      LibCard($("#search_keyword").val());
+    }
+    return false;
+  });
+
   // Listen to the current Auth state
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
@@ -101,63 +118,216 @@ async function main() {
     return false;
   });
   */
+}
 
-  function ModeSelection() {
-    var url = document.location.href;
-    var mode = findGetParameter("mode");
-    //console.log(url);
-    //console.log(window.location.search.substr(1));
-    //console.log();
-    switch (mode) {
-      case "user":
-        UserList();
-        break;
-      case "latest":
-        Latest();
-        break;
-      case "import":
-        Import();
-        break;
-    }
-    //UserList();
+// function parts
+
+function popUserEdit() {
+  console.log("popUserEdit");
+}
+
+function ModeSelection() {
+  var url = document.location.href;
+  var mode = findGetParameter("mode");
+  //console.log(url);
+  //console.log(window.location.search.substr(1));
+  //console.log();
+  switch (mode) {
+    case "latest":
+      Latest();
+      break;
+    case "libcard":
+      LibCard();
+      break;
+    case "import":
+      Import();
+      break;
+    default:
+      Welcome();
   }
+  //UserList();
+}
 
-  function findGetParameter(parameterName) {
-    var result = null,
-      tmp = [];
-    location.search
-      .substr(1)
-      .split("&")
-      .forEach(function(item) {
-        tmp = item.split("=");
-        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+function Welcome() {
+  welcome.style.display = "block";
+}
+
+function findGetParameter(parameterName) {
+  var result = null,
+    tmp = [];
+  location.search
+    .substr(1)
+    .split("&")
+    .forEach(function(item) {
+      tmp = item.split("=");
+      if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    });
+  return result;
+}
+
+function LibCard(searchKeyword) {
+  dataTable.innerHTML = "";
+  var html_table =
+    "<i>* Only the 50 latest registered records are shown.</i><br><br>";
+  html_table += '<table width="100%" id="tbUsers">';
+  html_table += "<tr>";
+  html_table += "  <td><b>No.</b></td>";
+  html_table += "  <td><b>Card No.</b></td>";
+  html_table += "  <td><b>Email</b></td>";
+  html_table += "</tr>";
+
+  //.orderBy("dateCreated", "desc")
+  //.limit(50)
+  var key = searchKeyword + "";
+  key = key.toLowerCase();
+  if (key.length > 3 && key != "undefined") {
+    latestActivitiesListener = firebase
+      .firestore()
+      .collection("libraryCards")
+      .orderBy("no", "desc")
+      .onSnapshot(snaps => {
+        var trData = "";
+        snaps.forEach(doc => {
+          if (
+            doc
+              .data()
+              .email.toLowerCase()
+              .includes(key) ||
+            doc
+              .data()
+              .cardNo.toLowerCase()
+              .includes(key)
+          ) {
+            trData +=
+              "<tr><td>" +
+              doc.data().no +
+              "</td><td>" +
+              doc.data().cardNo +
+              "</td><td>" +
+              doc.data().email +
+              "</td><td>" +
+              " <label> <a href='#' onclick='popUserEdit(1);'>&#9998;</a></label>  <label><a href='#'>&#9003;</a></label> " +
+              "</td></tr>";
+          }
+        });
+        trData += '<tr><td align="center" colspan="3">End of Records</td></tr>';
+        html_table += trData + "</table>";
+        dataTable.innerHTML = html_table;
+        usersearch.style.display = "block";
       });
-    return result;
+  } else {
+    latestActivitiesListener = firebase
+      .firestore()
+      .collection("libraryCards")
+      .orderBy("no", "desc")
+      .limit(50)
+      .onSnapshot(snaps => {
+        var trData = "";
+        snaps.forEach(doc => {
+          trData +=
+            "<tr><td>" +
+            doc.data().no +
+            "</td><td>" +
+            doc.data().cardNo +
+            "</td><td>" +
+            doc.data().email +
+            "</td><td>" +
+            " <label> <a href='javascript:popUserEdit(2);'>&#9998;</a></label>  <label><a href='#'>&#9003;</a></label> " +
+            "</td></tr>";
+        });
+        trData += '<tr><td align="center" colspan="3">End of Records</td></tr>';
+        html_table += trData + "</table>";
+        dataTable.innerHTML = html_table;
+        usersearch.style.display = "block";
+      });
   }
+}
 
-  function UserList() {}
+function Latest(searchKeyword) {
+  dataTable.innerHTML = "";
+  var html_table =
+    "<i>* Only the 50 latest registered records are shown.</i><br><br>";
+  html_table += '<table width="100%" id="tbUsers">';
+  html_table += "<tr>";
+  html_table += "  <td><b>Name</b></td>";
+  html_table += "  <td><b>Card No.</b></td>";
+  html_table += "  <td><b>Email</b></td>";
+  html_table += "  <td><b>Gender</b></td>";
+  html_table += "  <td><b>IC No.</b></td>";
+  html_table += "  <td><b>Created</b></td>";
+  html_table += "  <td><b>Tools</b></td>";
+  html_table += "</tr>";
 
-  function Latest() {
-    var html_table = '* Only 50 Records <table width="100%" cellpadding="5" style="font-size:12px;" >';
-    html_table += "<tr>";
-    html_table += "  <td><b>Name</b></td>";
-    html_table += "  <td><b>Card No.</b></td>";
-    html_table += "  <td><b>Email</b></td>";
-    html_table += "  <td><b>Gender</b></td>";
-    html_table += "  <td><b>IC No.</b></td>";
-    html_table += "  <td><b>Created</b></td>";
-    html_table += "</tr>";
-
+  //.orderBy("dateCreated", "desc")
+  //.limit(50)
+  var key = searchKeyword + "";
+  key = key.toLowerCase();
+  if (key.length > 3 && key != "undefined") {
+    latestActivitiesListener = firebase
+      .firestore()
+      .collection("users")
+      .orderBy("dateCreated", "desc")
+      .onSnapshot(snaps => {
+        var trData = "";
+        snaps.forEach(doc => {
+          if (
+            doc
+              .data()
+              .name.toLowerCase()
+              .includes(key) ||
+            doc
+              .data()
+              .email.toLowerCase()
+              .includes(key) ||
+            doc
+              .data()
+              .icNo.toLowerCase()
+              .includes(key) ||
+            doc
+              .data()
+              .cardNo.toLowerCase()
+              .includes(key) ||
+            doc
+              .data()
+              .dateCreated.toDate()
+              .toDateString()
+              .toLowerCase()
+              .includes(key)
+          ) {
+            trData +=
+              "<tr><td>" +
+              doc.data().name +
+              "</td><td>" +
+              doc.data().cardNo +
+              "</td><td>" +
+              doc.data().email +
+              "</td><td>" +
+              doc.data().gender +
+              "</td><td>" +
+              doc.data().icNo +
+              "</td><td>" +
+              doc
+                .data()
+                .dateCreated.toDate()
+                .toDateString() +
+              "</td><td>" +
+              " <label> <a href='#' onclick='popUserEdit(1);'>&#9998;</a></label>  <label><a href='#'>&#9003;</a></label> " +
+              "</td></tr>";
+          }
+        });
+        trData += '<tr><td align="center" colspan="6">End of Records</td></tr>';
+        html_table += trData + "</table>";
+        dataTable.innerHTML = html_table;
+        usersearch.style.display = "block";
+      });
+  } else {
     latestActivitiesListener = firebase
       .firestore()
       .collection("users")
       .orderBy("dateCreated", "desc")
       .limit(50)
       .onSnapshot(snaps => {
-        // Reset page
-        latest.innerHTML = "";
         var trData = "";
-        // Loop through documents in database
         snaps.forEach(doc => {
           trData +=
             "<tr><td>" +
@@ -171,25 +341,22 @@ async function main() {
             "</td><td>" +
             doc.data().icNo +
             "</td><td>" +
-            doc.data().dateCreated.toDate().toDateString() +
+            doc
+              .data()
+              .dateCreated.toDate()
+              .toDateString() +
+            "</td><td>" +
+            " <label> <a href='javascript:popUserEdit(2);'>&#9998;</a></label>  <label><a href='#'>&#9003;</a></label> " +
             "</td></tr>";
-
-          //const entry = document.createElement("tr");
-          //entry.textContent = doc.data().cardNo + ": " + doc.data().name;
         });
+        trData += '<tr><td align="center" colspan="6">End of Records</td></tr>';
         html_table += trData + "</table>";
-        latest.innerHTML = html_table;
+        dataTable.innerHTML = html_table;
+        usersearch.style.display = "block";
       });
   }
-
-  function Import() {}
-
-  function unsubscribeGuestbook() {
-    if (latestActivitiesListener != null) {
-      latestActivitiesListener();
-      latestActivitiesListener = null;
-    }
-  }
 }
+
+function Import() {}
 
 main();
